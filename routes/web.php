51631d0b756438666518a53f8e51ad75effa3ef6
Weje Emmanuel <wejeemmanuel34@gmail.com>
2020-07-24 23:02:27 +0100

    added home page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('search', 'YoutubeController@search');
+
+Route::get('/', 'HomeController@index');
