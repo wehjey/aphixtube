4591cc4c8a0f0ed090423378db0c1321124740c7
Weje Emmanuel <wejeemmanuel34@gmail.com>
2020-07-25 03:51:27 +0100

    refactored youtube controller to use a repository

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -2,48 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Repositories\YoutubeRepository;
 
 class YoutubeController extends Controller
 {
-
-    private $_MAX_RESULTS = 12; // Default number of results
-
+    
     /**
      * Function to search query youtube API
      * Search keyword and page token are optional params and only used if available
-     *
-     * @param Request $request contain query params
+     * 
+     * @param \App\Repositories\YoutubeRepository $youtubeRepository
      * 
      * @return json
      */
-    public function search(Request $request) 
+    public function search(YoutubeRepository $youtubeRepository) 
     {
-        $baseUrl = config('app.youtube_url');
-        $apiKey = config('app.youtube_api_key');
-
-        $requestUrl = "{$baseUrl}/search?key={$apiKey}&part=snippet&maxResults={$this->_MAX_RESULTS}";
-
-        if (request()->has('query') && request('query') != '') {
-            $requestUrl .= '&q=' . urlencode($request['query']);
-        }
-
-        if (request()->has('pageToken') && request('pageToken') != '') {
-            $requestUrl .= "&pageToken={$request['pageToken']}";
-        }
-
-        $response = makeRequest($requestUrl, 'GET');
-
-        if ($response['success'] && !isset($response['data']['error'])) {
-            return response()->json($response);
-        }
-
-        return response()->json(
-            [
-                'success' => false,
-                'data' => null,
-                'error' => 'No search result(s) available'
-            ]
-        );
+        $data = $youtubeRepository->search(request()->only(['query', 'pageToken']));
+        return response()->json($data);
     }
 }
